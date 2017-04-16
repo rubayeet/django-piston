c3b982c376e3e11e88c501e33af27bf7de7a4df7
rubayeet <rubayeet@newscred.com>
2017-04-16 11:49:20 +0600

    Deprecate lookup functionality

--- piston/store.py
+++ piston/store.py
@@ -1,7 +1,14 @@
 import oauth
 
-from models import Nonce, Token, Consumer
-from models import generate_random, VERIFIER_SIZE
+from django.contrib.auth.models import User
+
+SECRET_SIZE = 32
+VERIFIER_SIZE = 10
+
+
+def generate_random(length=SECRET_SIZE):
+    return User.objects.make_random_password(length=length)
+
 
 class DataStore(oauth.OAuthDataStore):
     """Layer between Python OAuth and Django database."""
@@ -11,59 +18,19 @@ class DataStore(oauth.OAuthDataStore):
         self.scope = oauth_request.parameters.get('scope', 'all')
 
     def lookup_consumer(self, key):
-        try:
-            self.consumer = Consumer.objects.get(key=key)
-            return self.consumer
-        except Consumer.DoesNotExist:
-            return None
+        pass
 
     def lookup_token(self, token_type, token):
-        if token_type == 'request':
-            token_type = Token.REQUEST
-        elif token_type == 'access':
-            token_type = Token.ACCESS
-        try:
-            self.request_token = Token.objects.get(key=token,
-                                                   token_type=token_type)
-            return self.request_token
-        except Token.DoesNotExist:
-            return None
+        pass
 
     def lookup_nonce(self, oauth_consumer, oauth_token, nonce):
-        if oauth_token is None:
-            return None
-        nonce, created = Nonce.objects.get_or_create(consumer_key=oauth_consumer.key,
-                                                     token_key=oauth_token.key,
-                                                     key=nonce)
-        if created:
-            return None
-        else:
-            return nonce.key
+        pass
 
     def fetch_request_token(self, oauth_consumer, oauth_callback):
-        if oauth_consumer.key == self.consumer.key:
-            self.request_token = Token.objects.create_token(consumer=self.consumer,
-                                                            token_type=Token.REQUEST,
-                                                            timestamp=self.timestamp)
-
-            if oauth_callback:
-                self.request_token.set_callback(oauth_callback)
-
-            return self.request_token
-        return None
+        pass
 
     def fetch_access_token(self, oauth_consumer, oauth_token, oauth_verifier):
-        if oauth_consumer.key == self.consumer.key \
-        and oauth_token.key == self.request_token.key \
-        and (not oauth_verifier
-             or oauth_verifier == self.request_token.verifier) \
-        and self.request_token.is_approved:
-            self.access_token = Token.objects.create_token(consumer=self.consumer,
-                                                           token_type=Token.ACCESS,
-                                                           timestamp=self.timestamp,
-                                                           user=self.request_token.user)
-            return self.access_token
-        return None
+        pass
 
     def authorize_request_token(self, oauth_token, user):
         if oauth_token.key == self.request_token.key:
